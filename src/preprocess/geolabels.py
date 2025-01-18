3b100e9b04c472278b3f5aab5d24470c98f6f913
Scott Spicer <scottspicer24@gmail.com>
2025-01-18 17:17:00 -0500

    added csv of countries and cities

--- src/preprocess/geolabels.py
+++ src/preprocess/geolabels.py
@@ -2,4 +2,36 @@ import csv
 
 def main():
 
-    path = './worldcities.csv'
+    in_path = './src/preprocess/worldcities.csv'
+    out_path = './src/preprocess/geolabels.csv'
+
+    map = {}
+
+    with open(in_path, mode='r', newline='', encoding='utf-8') as file:
+        csvFile = csv.reader(file)
+        i = 0
+        for line in csvFile:
+            #print(line[1], " : ", line[4])
+            country = line[4]
+            city = line[1]
+            if country in map:
+                map[country].append(city)
+            else:
+                map[country] = [city]
+
+
+    with open(out_path, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        
+        for key, value in map.items():
+            entry = [key]
+            for i, city in enumerate(value):
+                '''if i == 30:
+                    break'''
+                entry.append(city)
+
+            # Write attribute values to csv
+            writer.writerow([entry])
+
+if __name__ == "__main__":
+    main()
